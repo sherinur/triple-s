7864d2849f604832f3d0126080efa801e0002510
sherinur <sherinurislam@gmail.com>
2024-10-15 17:30:34 +0500

    Implement logging and config

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,27 @@ import (
 	"flag"
 	"log"
 	"net/http"
-
 	"triple-s/pkg/logger"
 )
+
+func logRequestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[REQUEST] %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr) // Логируем метод, путь и IP-адрес
+		next.ServeHTTP(w, r)                                                      // Вызываем следующий обработчик
+	})
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("SHERIII SHERIII GOOO"))
 }
 
+func gay(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("YOU ARE GAY"))
+}
+
 func main() {
+	var err error
+
 	port := flag.String("port", "4400", "Port number")
 	dir := flag.String("dir", ".", "Path to the directory")
 
@@ -22,8 +34,13 @@ func main() {
 	logger.PrintfInfoMsg("Path to the directory set: " + *dir)
 
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
-	err := http.ListenAndServe(":"+*port, mux)
+	mux.HandleFunc("/gay", gay)
+
+	loggedMux := logRequestMiddleware(mux)
+
+	err = http.ListenAndServe(":"+*port, loggedMux)
 	if err != nil {
 		log.Fatal(err)
 	}
